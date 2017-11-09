001b4b724b290c0b69f513ec953e9f3085271e65
Doug Emery <emeryr@upenn.edu>
2017-11-09 16:02:30 -0500

    Update certainty delegates

--- app/models/quire_leaf.rb
+++ app/models/quire_leaf.rb
@@ -7,18 +7,21 @@ class QuireLeaf < ActiveRecord::Base
   validates :leaf, presence: true
   validates :quire, presence: true
 
-  SUBQUIRES = [ [ 'Main', 0 ] ] + (1..10).map { |i| [ i, i ] }
+  SUBQUIRES = ([['Main', 0]] + (1..10).map { |i| [i, i] }).freeze
 
   acts_as_list scope: :quire
 
   accepts_nested_attributes_for :leaf
 
-  delegate :single?,         to: :leaf,  prefix: true,  allow_nil: true
-  delegate :folio_number,    to: :leaf,  prefix: false, allow_nil: true
-  delegate :quire_uncertain, to: :leaf,  prefix: false, allow_nil: true
-  delegate :mode,            to: :leaf,  prefix: true,  allow_nil: true
-  delegate :number,          to: :quire, prefix: true,  allow_nil: true
-  delegate :xml_id,          to: :quire, prefix: true,  allow_nil: true
+  delegate :single?,                to: :leaf,  prefix: true,  allow_nil: true
+  delegate :single_certainty,       to: :leaf,  prefix: false, allow_nil: true
+  delegate :folio_number,           to: :leaf,  prefix: false, allow_nil: true
+  delegate :folio_number_certainty, to: :leaf,  prefix: false, allow_nil: true
+  delegate :quire_uncertain,        to: :leaf,  prefix: false, allow_nil: true
+  delegate :mode,                   to: :leaf,  prefix: true,  allow_nil: true
+  delegate :mode_certainty,         to: :leaf,  prefix: true,  allow_nil: true
+  delegate :number,                 to: :quire, prefix: true,  allow_nil: true
+  delegate :xml_id,                 to: :quire, prefix: true,  allow_nil: true
 
   def to_s
     "#{self.class.name}: quire: #{quire}; leaf: #{leaf}"

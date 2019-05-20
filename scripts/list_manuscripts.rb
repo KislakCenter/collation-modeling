e4a2718e52be8c3199ffcb36558b6810ca6b19c8
Doug Emery <emeryr@upenn.edu>
2019-05-20 12:00:01 -0400

    More flexible list_manuscripts script

--- scripts/list_manuscripts.rb
+++ scripts/list_manuscripts.rb
@@ -2,15 +2,18 @@
 
 exit # don't actually run; scratch code
 
+# URL = "https://biblio-philly-collations.herokuapp.com"
+URL = "https://protected-island-3361.herokuapp.com"
+
 require 'csv'
 rows = []
 header = %w{ id url shelfmark title created_at updated_at }
 CSV do |row|
 	row << header
-	Manuscript.all.each do |ms| 
+	Manuscript.all.each do |ms|
 		data = []
 		data << ms.id
-		data << "https://biblio-philly-collations.herokuapp.com/manuscripts/xml/#{ms.id}?xml_type=filled_quires"
+		data << "#{URL}/manuscripts/xml/#{ms.id}?xml_type=filled_quires"
 		data << ms.shelfmark
 		data << ms.title
 		data << ms.created_at
